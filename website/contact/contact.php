0cfdeca58098ae098dd7760e4ad5ebad5e6a39a0
PRAS Martin <martin.pras@epitech.eu>
2018-04-20 08:25:46 +0200

    Website serveur setup

--- website/contact/contact.php
+++ website/contact/contact.php
@@ -2,21 +2,21 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
   $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
-  $message = trim($_POST["message"]);
+  $message = trim($_POST["msg"]);
 
   if (empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
     http_response_code(400);
     exit;
   }
 
-  $recipient = "martin.pras@epitech.eu";
+  $recipient = "meetzemup.dev@gmail.com";
 
-  $subject = "New contact form";
+  $subject = "New MZU Contact Form";
 
   $email_content .= "Email: $email\n\n";
   $email_content .= "Message:\n$message\n";
 
-  $email_headers = "From: $name <$email>";
+  $email_headers = "From: <$email>";
 
   if (mail($recipient, $subject, $email_content, $email_headers)) {
     http_response_code(200);
